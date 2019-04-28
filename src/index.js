240d911185e55e5b687ba2dba009a6de427297f5
Christiaan Hemerik <christiaan@aanzee.nl>
2019-04-28 02:13:50 +0200

    Imports scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Dialog from './library/components/Dialog/Dialog';
 import Button from './library/controls/Button/Button';
 import Link from './library/controls/Link/Link';
+import './scss/main.scss';
 
 export {
     Dialog,
