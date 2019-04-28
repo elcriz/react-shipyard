5e3093126316e0f819f3322146ccbe2ae8c63a95
Christiaan Hemerik <christiaan@aanzee.nl>
2019-04-28 08:08:20 +0200

    Minor update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import Dialog from './library/components/Dialog/Dialog';
 import Button from './library/controls/Button/Button';
 import Link from './library/controls/Link/Link';
-import './scss/main.scss';
+
+require('./scss/main.scss');
 
 export {
     Dialog,
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,52 @@
 const webpack = require('webpack');
 const path = require('path');
 const autoprefixer = require('autoprefixer');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ReplacePlugin = require('webpack-plugin-replace');
+const ReplaceInFileWebpackPlugin = require('replace-in-file-webpack-plugin');
+const StringReplacePlugin = require('string-replace-webpack-plugin');
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
-  entry: './index.js',
+  entry: ['./index.js', './scss/main.scss'],
   output: {
     path: path.resolve(__dirname, 'lib'),
     filename: 'index.js',
     library: ['react-shipyard'],
     libraryTarget: 'umd',
     publicPath: '/lib/',
-    globalObject: `typeof self !== 'undefined' ? self : this`,
+    globalObject: `typeof self !== 'undefined' ? self : this`
   },
+  devtool: 'source-map',
+  mode: isProduction ? 'production' : 'development',
   module: {
     rules: [
+        {
+            test: /\.s?[ac]ss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                { loader: 'css-loader', options: { url: false, sourceMap: true } },
+                { loader: 'sass-loader', options: { sourceMap: true } }
+            ],
+        },
       {
-        test: /\.(js|jsx)$/,
+        test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: 'babel-loader'
       },
-      {
-        test: /\.scss$/,
-        use: [
-          "style-loader",
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader"
-        ]
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader?modules',
-        include: /flexboxgrid/
-      },
+    //   {
+    //     test: /\.scss$/,
+    //     use: [
+    //       process.env.NODE_ENV === 'production'
+    //         ? 'style-loader'
+    //         : MiniCssExtractPlugin.loader,
+    //       'css-loader',
+    //       'sass-loader'
+    //     ]
+    //   },
       {
         test: /\.(ttf|eot|woff(2)?)(\?[a-z0-9=&.]+)?$/,
         use: ['file-loader']
@@ -46,10 +58,12 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx', '.scss']
   },
   plugins: [
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+        filename: 'es/css/[name].css'
+    }),
     new webpack.LoaderOptionsPlugin({
       options: {
         context: __dirname,
