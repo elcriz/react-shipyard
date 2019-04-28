6200aecb45b12908a0f3eb736ae25bd09fc8680b
Christiaan Hemerik <christiaan@aanzee.nl>
2019-04-28 21:23:29 +0200

    Adds components to styleguide

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import { Breadcrumb } from './library/components/Breadcrumb/Breadcrumb';
 import { Dialog } from './library/components/Dialog/Dialog';
 import { Button } from './library/controls/Button/Button';
+import { DropDownMenu } from './library/controls/DropDownMenu/DropDownMenu';
 import { Link } from './library/controls/Link/Link';
+import { TextInput } from './library/controls/TextInput/TextInput';
 
 if (process.env.NODE_ENV === 'development') {
     require('./scss/main.scss');
@@ -9,7 +12,10 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 export {
+    Breadcrumb,
     Dialog,
     Button,
-    Link
+    DropDownMenu,
+    Link,
+    TextInput
 };
