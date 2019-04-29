500ddaa22a2ce63f777356e89506317e8673610e
Christiaan Hemerik <Christiaan.Hemerik@thelearningnetwork.nl>
2019-04-29 23:24:39 +0200

    Adds new SegmentedControl control component

---
+++ src/library/controls/SegmentedControl/SegmentedControl.js
@@ -0,0 +1,113 @@
+import React, { useState }  from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+export const SegmentedControl = ({
+    label,
+    options,
+    value,
+    onChange,
+    isSmall,
+    isInline,
+    ...props
+}) => {
+    const [currentValue, setValue] = useState(value || '');
+
+    const handleChange = (newValue) => {
+        if (onChange) {
+            onChange(newValue);
+        }
+        setValue(newValue);
+    };
+
+    return (
+        <div
+            {...props}
+            className={classnames('segmented-control', {
+                'segmented-control--inline': isInline,
+                'segmented-control--small': isSmall,
+            })}
+        >
+            {label && (
+                <div className="segmented-control__label-box">
+                    <label className="segmented-control__label">
+                        {label}
+                    </label>
+                </div>
+            )}
+            <div className="segmented-control__items">
+                {options.map((optionValue, index) => {
+                    const serializedOptionValue = typeof optionValue === 'object' ? Object.keys(optionValue)[0] : optionValue;
+                    const inputLabel = typeof optionValue === 'object' ? optionValue[Object.keys(optionValue)] : optionValue;
+
+                    return (
+                        <label
+                            key={index}
+                            className={`segmented-control__item${value === serializedOptionValue ? ' is-selected' : ''}`}
+                        >
+                            <input
+                                className="segmented-control__input"
+                                value={serializedOptionValue}
+                                checked={currentValue === serializedOptionValue}
+                                type="radio"
+                                onChange={(event) => {
+                                    handleChange(event.target.value);
+                                }}
+                            />
+                            <span className="segmented-control__input-label">
+                                {inputLabel}
+                            </span>
+                        </label>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+SegmentedControl.propTypes = {
+
+    /**
+     * SegmentedControl field label.
+     */
+    label: PropTypes.string,
+
+    /**
+     * Options.
+     * Either `['Value 1', 'Value 2']` or `[{ 'key': 'value' }]`
+     */
+    options: PropTypes.arrayOf(
+        PropTypes.oneOf([
+            PropTypes.string,
+            PropTypes.object
+        ])
+    ).isRequired,
+
+    /**
+     * Current value.
+     */
+    value: PropTypes.string,
+
+    /**
+     * Gets called when the user changes the value.
+     * @param {any} event The selected value
+     */
+    onChange: PropTypes.func.isRequired,
+
+    /**
+     * Display the segmented control in a smaller version.
+     */
+    isSmall: PropTypes.bool,
+
+    /**
+     * Should the segmented control be rendered inline (e.g. inside another form field) or not.
+     */
+    isInline: PropTypes.bool,
+};
+
+SegmentedControl.defaultProps = {
+    isSmall: false,
+    isInline: false
+};
+
+export default SegmentedControl;
---
+++ src/library/controls/SegmentedControl/SegmentedControl.md
@@ -0,0 +1,63 @@
+SegmentedControl example:
+
+```js
+<SegmentedControl
+    label="This is a label"
+    options={['Option A', 'Option B', 'Option C']}
+    value='Option A'
+    onChange={(selectedValue) => {
+        console.log('Segmented control value got changed!', selectedValue);
+    }}
+/>
+```
+
+SegmentedControl with key-value pair options (provided as prop `options`, see code):
+
+```js
+<SegmentedControl
+    label="This is a label"
+    options={[
+        { 'A': 'Option A' },
+        { 'B': 'Option B' },
+        { 'C': 'Option C' }
+    ]}
+    value='A'
+    onChange={(selectedValue) => {
+        console.log('Segmented control value got changed!', selectedValue);
+    }}
+/>
+```
+
+Inline SegmentedControl:
+
+```js
+<SegmentedControl
+    label="This is a label"
+    options={['Option A', 'Option B', 'Option C']}
+    value='Option A'
+    isInline
+/>
+```
+
+SegmentedControl in smaller size:
+
+```js
+<SegmentedControl
+    label="This is a label"
+    options={['Option A', 'Option B', 'Option C']}
+    value='Option A'
+    isSmall
+/>
+```
+
+Inline SegmentedControl in smaller size:
+
+```js
+<SegmentedControl
+    label="This is a label"
+    options={['Option A', 'Option B', 'Option C']}
+    value='Option A'
+    isInline
+    isSmall
+/>
+```
